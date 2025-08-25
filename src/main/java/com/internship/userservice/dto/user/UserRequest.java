aba1d4c0412a99210c34989f430c7df2bada9b3b
Maksim Asonau <zabungalilovo@gmail.com>
2025-08-25 22:54:03 +0300

    fix minor

--- src/main/java/com/internship/userservice/dto/user/UserRequest.java
+++ src/main/java/com/internship/userservice/dto/user/UserRequest.java
@@ -8,15 +8,11 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Data
-@Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
