74bad41a1b9126cd342495a5a129cce81d6b5d0f
Maksim Asonau <zabungalilovo@gmail.com>
2025-08-28 01:13:22 +0300

    minor fix

--- src/main/java/com/internship/userservice/service/UserService.java
+++ src/main/java/com/internship/userservice/service/UserService.java
@@ -8,21 +8,12 @@ import java.util.List;
 public interface UserService {
 
     UserResponse create(UserRequest dto, Long userCredentialsId);
-
     UserResponse getUserById(Long id);
-
     UserResponse getUserByEmail(String email);
-
     List<UserResponse> getUsersByIds(List<Long> ids);
-
     UserResponse updateUserById(Long id, UserRequest dto, Long userCredentialsId);
-
     void deleteUserById(Long id, Long userCredentialsId);
-
     List<UserResponse> getAllUsers();
-
     UserResponse getByUserCredentialsId(Long userCredentialsId);
-
-
 }
 
